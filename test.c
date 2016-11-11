2c2c28932fadc389bc43407bdf9e0a98aa8e11a1
AI009 <AI009@163.com>
2016-11-11 19:45:32 +0800

    test

---
+++ test.c
@@ -0,0 +1,20 @@
+/* ************************************************************************
+ *       Filename:  test.c
+ *    Description:  
+ *        Version:  1.0
+ *        Created:  2016年11月11日 19时43分36秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *         Author:  YOUR NAME (), 
+ *        Company:  
+ * ************************************************************************/
+
+#include<stdio.h>
+
+int main()
+{
+	printf("hello github\n");
+	return 0;
+}
+
+
