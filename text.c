231b8610ee150b4bbb7243b1b62674fc83608e12
AI009 <AI009@163.com>
2016-11-13 21:17:46 +0800

    v1.0

---
+++ text.c
@@ -0,0 +1,20 @@
+/* ************************************************************************
+ *       Filename:  text.c
+ *    Description:  
+ *        Version:  1.0
+ *        Created:  2016年11月13日 21时16分56秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *         Author:  YOUR NAME (), 
+ *        Company:  
+ * ************************************************************************/
+
+#include<stdio.h>
+
+int main()
+{
+	printf("hello shell\n");
+	return 0;
+}
+
+
